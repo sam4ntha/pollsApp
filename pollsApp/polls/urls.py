ec3f002299391e02aa620c20dd14a5b0e2b7367d
Samantha <stap0428@gmail.com>
2024-05-31 14:15:36 -0600

    05 Agregando estilos

--- pollsApp/polls/urls.py
+++ pollsApp/polls/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('<int:q_id>/vote/', views.vote, name='vote'),
     path('<int:q_id>/results/', views.results, name='results'),
+    path('results/<int:q_id>/chart/', views.results_chart, name='results_chart'),
 ]
--- pollsApp/polls/views.py
+++ pollsApp/polls/views.py
@@ -1,5 +1,7 @@
-from django.http import Http404
+from io import BytesIO
+from django.http import Http404, HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
+import matplotlib.pyplot as plt
 
 from .models import Choice, Question
 # Create your views here.
@@ -15,6 +17,7 @@ def vote(request, q_id):
             c = Choice.objects.get(id=choice)
             c.votes += 1
             c.save()
+            
             return redirect('polls:results', q_id)
         except (KeyError):
             return render(request, 'polls/question.html', { "question": question, "error_message": "¡Debes seleccionar una opción!"})
@@ -27,3 +30,20 @@ def results(request, q_id):
     except Question.DoesNotExist:
         raise Http404("¡No existe la encuesta!")
     return render(request, 'polls/results.html', { "question": question })
+
+def results_chart(request, q_id):
+    question = get_object_or_404(Question, id=q_id)
+    choices = question.choice_set.all()
+    labels = [c.choice_text for c in choices]
+    sizes = [c.votes for c in choices]
+
+    fig, ax = plt.subplots()
+    ax.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+    ax.axis('equal')  # Para que el gráfico sea un círculo
+
+    buf = BytesIO()
+    plt.savefig(buf, format='png')
+    plt.close(fig)
+    buf.seek(0)
+
+    return HttpResponse(buf.getvalue(), content_type='image/png')
